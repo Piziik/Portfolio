30660aca0c95c165cb957ec1d425c3228b0a3fa9
Gabrielle Doffe <gabydoffe@gmail.com>
2022-03-15 13:19:13 +0100

    ça fonctionne

--- assets/js/connexion.js
+++ assets/js/connexion.js
@@ -1,41 +1,28 @@
-/*var id = document.getElementById('pass');
+//Fonction animation input
+window.addEventListener('DOMContentLoaded', event => {
 
-var typewriter = new Typewriter(id, {
-    loop: true
-});
+    let id = document.querySelector('#pass');
+    let id_placeholder = 'CoinCoin';
 
-typewriter.typeString('CoinCoin')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
-    .start();
-    */
-
-let id = document.querySelector('#pass');
-let id_placeholder = 'CoinCoin';
-
-id.addEventListener('input', (e)=>{
-    if(e.target.value.length < 1){
-        typing();s
-    }
-})
-
-function typing(){
-    let placeholder = '';
-    let x = 0;
-    id.setAttribute('placeholder', '');
-    let interval = setInterval(() =>{
-        placeholder += id_placeholder[x];
-        x++;
-
-        id.setAttribute('placeholder', placeholder);
-        if(x > id_placeholder.length - 1){
-            clearInterval(interval);
+    id.addEventListener('input', (e)=>{
+        if(e.target.value.length < 1){
+            typing();
         }
-    },120)
-}
-typing();
+    })
+
+    function typing(){
+        let placeholder = '';
+        let x = 0;
+        id.setAttribute('value', '');
+        let interval = setInterval(() =>{
+            placeholder += id_placeholder[x];
+            x++;
+    
+            id.setAttribute('value', placeholder);
+            if(x > id_placeholder.length - 1){
+                clearInterval(interval);
+            }
+        },200)
+    }
+    typing();
+})
